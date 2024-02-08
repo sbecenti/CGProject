f5cdf668c40d59d3b04f15051ecd398c9953fb99
SpencerM-2k1 <122830768+SpencerM-2k1@users.noreply.github.com>
2024-02-08 12:45:10 -0700

    Update T4LQ1.cpp
    Pillars, concrete slope, unfinished green front sections

--- T4LQ1.cpp
+++ T4LQ1.cpp
@@ -11,19 +11,68 @@ void drawObjects() {
 
     glBegin(GL_QUADS);
 
-    // Front face of building (red)
+    // Front face of building
     glColor3f(0.6, 0.3, 0.0);
     glVertex3f(-17.5, -5.5, 5.0); // Bottom-left
     glVertex3f(7.5, -5.5, 5.0);   // Bottom-right
     glVertex3f(7.5, 5.0, 5.0);    // Top-right
     glVertex3f(-17.5, 5.0, 5.0);  // Top-left
 
-    // Right face of building (blue)
+    // Right face of building
     glColor3f(0.4, 0.2, 0.0);
     glVertex3f(7.5, -5.5, 5.0);    // Bottom-front
     glVertex3f(7.5, -5.5, -55.0);  // Bottom-back
     glVertex3f(7.5, 5.0, -55.0);   // Top-back
     glVertex3f(7.5, 5.0, 5.0);     // Top-front
+
+    //Corner Pillar (left-front)
+    glColor3f(0.6, 0.2, 0.2);//Front face
+    glVertex3f(6.0, -5.5, 6.0); // Bottom-left
+    glVertex3f(9.0, -5.5, 6.0); // Bottom-right
+    glVertex3f(9.0, 5.0, 6.0);  // Top-right
+    glVertex3f(6.0, 5.0, 6.0);  // Top-left
+
+    //glColor3f(0.6, 0.2, 0.2);//Right face
+    glVertex3f(9.0, -5.5, 6.0); // Bottom-left
+    glVertex3f(9.0, -5.5, 2.0); // Bottom-right
+    glVertex3f(9.0, 5.0, 2.0);  // Top-right
+    glVertex3f(9.0, 5.0, 6.0);  // Top-left
+
+    //Corner Pillar (right-front, red front, blue side)
+    glColor3f(0.6, 0.2, 0.2);//Front face
+    glVertex3f(-19.0, -5.5, 6.0); // Bottom-left
+    glVertex3f(-16.0, -5.5, 6.0); // Bottom-right
+    glVertex3f(-16.0, 5.0, 6.0);  // Top-right
+    glVertex3f(-19.0, 5.0, 6.0);  // Top-left
+
+    //glColor3f(0.6, 0.2, 0.2);//Right face
+    glVertex3f(-16.0, -5.5, 6.0); // Bottom-left
+    glVertex3f(-16.0, -5.5, 5.0); // Bottom-right
+    glVertex3f(-16.0, 5.0, 5.0);  // Top-right
+    glVertex3f(-16.0, 5.0, 6.0);  // Top-left
+
+    // Front face green sections (4 total)
+    int interval = (22.0)/9.0; //6-(-16) = distance between pillars
+    glColor3f(0.0, 1.0, 0.0);
+    glVertex3f(-16.0 + interval * 1.0, -5.5, 5.01); // Bottom-left
+    glVertex3f(-16.0 + interval * 2.0, -5.5, 5.01);   // Bottom-right
+    glVertex3f(-16.0 + interval * 2.0, 5.0, 5.01);    // Top-right
+    glVertex3f(-16.0 + interval * 1.0, 5.0, 5.01);  // Top-left
+
+    glVertex3f(-16.0 + interval * 4.0, -5.5, 5.01); // Bottom-left
+    glVertex3f(-16.0 + interval * 5.0, -5.5, 5.01);   // Bottom-right
+    glVertex3f(-16.0 + interval * 5.0, 5.0, 5.01);    // Top-right
+    glVertex3f(-16.0 + interval * 4.0, 5.0, 5.01);  // Top-left
+
+    glVertex3f(-16.0 + interval * 7.0, -5.5, 5.01); // Bottom-left
+    glVertex3f(-16.0 + interval * 8.0, -5.5, 5.01);   // Bottom-right
+    glVertex3f(-16.0 + interval * 8.0, 5.0, 5.01);    // Top-right
+    glVertex3f(-16.0 + interval * 7.0, 5.0, 5.01);  // Top-left
+
+    glVertex3f(-16.0 + interval * 10.0, -5.5, 5.01); // Bottom-left
+    glVertex3f(-16.0 + interval * 11.0, -5.5, 5.01);   // Bottom-right
+    glVertex3f(-16.0 + interval * 11.0, 5.0, 5.01);    // Top-right
+    glVertex3f(-16.0 + interval * 10.0, 5.0, 5.01);  // Top-left
 
     glEnd();
 
@@ -40,7 +89,15 @@ void drawObjects() {
     glVertex3f(-27.5, -6.5, 5.0);   // Top-left
     glEnd();
     
-
+    //Concrete Slope
+    glBegin(GL_QUADS);
+    glColor3f(0.5, 0.5, 0.5);
+    glVertex3f(4.0, -8.0, 15.01); // Bottom-left
+    glVertex3f(12.0, -8.0, 15.01);  // Bottom-right
+    glVertex3f(12.0, -6.5, 5.01);    // Top-right
+    glVertex3f(4.0, -6.5, 5.01);   // Top-left
+    glEnd();
+    
     glBegin(GL_QUADS);
 
     // Field floor (very bottom)
